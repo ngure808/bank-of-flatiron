6602ea5776a3d0f7af6d5858b64a4a12343d506d
ngure808 <mwangingure808@gmail.com>
2024-04-27 13:29:36 +0300

    array of transactions

---
+++ src/data/Transactions.js
@@ -0,0 +1,40 @@
+const Transactions = [
+    {
+        Date : "2024-04-10",
+        Descrition : "Dinner at Pizza hut",
+        Category : "Food",
+        Amount : -25
+    },
+    {
+        Date : "2024-04-27",
+        Descrition : "Skydiving Event",
+        Category : "Hobby",
+        Amount : -350
+    },
+    {
+        Date : "2024-04-01",
+        Descrition : "Monthly Paycheck",
+        Category : "Income",
+        Amount : 100000
+    },
+    {
+        Date : "2024-04-24",
+        Descrition : "Movie",
+        Category : "Entertainment",
+        Amount : -11.04
+    },
+    {
+        Date : "2024-04-10",
+        Descrition : "Stocked up the house",
+        Category : "Shopping",
+        Amount : -79.99
+    },
+    {
+        Date : "2024-04-16",
+        Descrition : "Took my car(PORSCHE 918 SPYDER)for service",
+        Category : "Maintenance",
+        Amount : -1000
+    }
+]
+
+export default Transactions;
