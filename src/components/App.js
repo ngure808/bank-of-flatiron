614893e77a037619c384288ba71f086709766c79
ngure808 <mwangingure808@gmail.com>
2024-04-27 17:41:30 +0300

    imported the transactions and added a header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-
+import Transactions from '../data/Transactions'
 import '../App.css';
+import DisplayContent from './DisplayContent';
 
 function App() {
   return (
     <div className="App">
-     
+      <h1 className='header'>The Royal Bank of Flatiron</h1>
+      <DisplayContent transactions = {Transactions}/>
     </div>
   );
 }
