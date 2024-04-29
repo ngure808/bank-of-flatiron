9f4858646aed54855b31a3a3ec16d42fbb70adfc
ngure808 <mwangingure808@gmail.com>
2024-04-29 08:52:44 +0300

    handles search bar,sort button and displaying the content

--- src/components/DisplayContent.js
+++ src/components/DisplayContent.js
@@ -1,29 +1,64 @@
 import React from 'react'
 import '../App.css'
+import SearchBar from './SearchBar';
+import { useState } from 'react';
 
 function DisplayContent({ transactions }) {
 
+  const sort = transactions.slice().sort((a, b) => a.date.localeCompare(b.date));
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOrder,setSortOrder] = useState('')
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSort = () => {
+    setSortOrder(sortOrder === 'alphabetically' ? 'reverse' : 'alphabetically');
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.category.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+
+  const sortedTransactions = filteredTransactions.slice().sort((a, b) => {
+    if (sortOrder === 'alphabetically') {
+      return a.description.localeCompare(b.description);
+    } else {
+      return a.date.localeCompare(b.date) 
+    }
+
+  });
+
     return (
-        <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map((expense, index) => (
-            <tr key={index}>
-              <td>{expense.Date}</td>
-              <td>{expense.Descrition}</td>
-              <td>{expense.Category}</td>
-              <td>{expense.Amount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <div>
+            <SearchBar searchTerm={searchTerm} onSearchChange={handleSearchChange}/>
+            <button onClick={handleSort}>Sort({sortOrder === 'alphabetically' ? 'BY DATE' : 'A-Z'})</button>
+            <div className='table-container'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sortedTransactions.map((expense, index) => (
+                    <tr key={index}>
+                        <td>{expense.date}</td>
+                        <td>{expense.description}</td>
+                        <td>{expense.category}</td>
+                        <td>{expense.amount}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
+        </div>
     )
 }
 
