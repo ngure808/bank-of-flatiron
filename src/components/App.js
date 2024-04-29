7bd860a94524c335af50fb2406047c7c25d53a32
ngure808 <mwangingure808@gmail.com>
2024-04-29 08:49:01 +0300

    imported state and addtransaction with a function as a prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import Transactions from '../data/Transactions'
 import '../App.css';
 import DisplayContent from './DisplayContent';
+import { useState } from 'react';
+import AddTransaction from './AddTransaction';
 
 function App() {
+const [transactions,setTransactions] = useState(Transactions)
+
+const handleAdd = (newTransaction) => {
+  setTransactions([...transactions,newTransaction])
+}
+
   return (
     <div className="App">
       <h1 className='header'>The Royal Bank of Flatiron</h1>
-      <DisplayContent transactions = {Transactions}/>
+      <AddTransaction onAddTransaction = {handleAdd}/>
+      <DisplayContent transactions = {transactions}/>
     </div>
   );
 }
