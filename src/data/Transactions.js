48669a07608ffe5b7bdb382cca0109c4e58e13cb
ngure808 <mwangingure808@gmail.com>
2024-04-29 08:38:37 +0300

    fixed a typo

--- src/data/Transactions.js
+++ src/data/Transactions.js
@@ -1,39 +1,39 @@
 const Transactions = [
     {
-        Date : "2024-04-10",
-        Descrition : "Dinner at Pizza hut",
-        Category : "Food",
-        Amount : -25
+        date : "2024-04-10",
+        description : "Dinner at Pizza hut",
+        category : "Food",
+        amount : -25
     },
     {
-        Date : "2024-04-27",
-        Descrition : "Skydiving Event",
-        Category : "Hobby",
-        Amount : -350
+        date : "2024-04-27",
+        description : "Skydiving Event",
+        category : "Hobby",
+        amount : -350
     },
     {
-        Date : "2024-04-01",
-        Descrition : "Monthly Paycheck",
-        Category : "Income",
-        Amount : 100000
+        date : "2024-04-01",
+        description : "Monthly Paycheck",
+        category : "Income",
+        amount : 100000
     },
     {
-        Date : "2024-04-24",
-        Descrition : "Movie",
-        Category : "Entertainment",
-        Amount : -11.04
+        date : "2024-04-24",
+        description : "Movie",
+        category : "Entertainment",
+        amount : -11.04
     },
     {
-        Date : "2024-04-10",
-        Descrition : "Stocked up the house",
-        Category : "Shopping",
-        Amount : -79.99
+        date : "2024-04-10",
+        description : "Stocked up the house",
+        category : "Shopping",
+        amount : -79.99
     },
     {
-        Date : "2024-04-16",
-        Descrition : "Took my car(PORSCHE 918 SPYDER)for service",
-        Category : "Maintenance",
-        Amount : -1000
+        date : "2024-04-16",
+        description : "Took my car(PORSCHE 918 SPYDER)for service",
+        category : "Maintenance",
+        amount : -1000
     }
 ]
 
